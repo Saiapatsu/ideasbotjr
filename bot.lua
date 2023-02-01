672c2bc117a67d64f220b66cff17e4b2d697444e
Saiapatsu <26184960+Saiapatsu@users.noreply.github.com>
2023-02-01 06:03:24 +0200

    Rearrange stuff

--- bot.lua
+++ bot.lua
@@ -1,17 +1,16 @@
-discordia = require "discordia"
-fs = require "fs"
-client = discordia.Client()
-
+-- hot reload
 local env = getfenv()
-
 function reload() setfenv(loadfile("funcs.lua"), env)() end
 reload()
 
+-- discordia one-time setup
+discordia = require "discordia"
+client = discordia.Client()
 client:on("ready", function() return onReady() end)
 client:on("messageCreate", function(message) return onMessageCreate(message) end)
+client:run(require("fs").readFileSync("./TOKEN"))
 
-client:run(fs.readFileSync("./TOKEN"))
-
+-- repl
 -- note: repl.lua has been modified to support passing an environment
 -- repl will require a bunch of stuff and put them in there by default
 local repl = require "repl" (process.stdin.handle, process.stdout.handle, "REPL active", env).start()
